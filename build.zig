490727034f8400dfab08f61a1017ec7e4273736c
batiati <rbatiati@gmail.com>
2022-11-14 22:02:33 -0300

    Adds tests
    - Adds unit tests for the JNI bindings, hosting a JVM on the zig process.
    - Fixes some minor bugs and compilers changes.
    - Adds missing Array functions.
    - build.zig changes to link the libjvm

--- build.zig
+++ build.zig
@@ -11,11 +11,34 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addSharedLibrary("jni_example", "test/demo.zig", .unversioned);
+    //Example
+    {
+        const lib = b.addSharedLibrary("jni_example", "test/demo.zig", .unversioned);
 
-    lib.addPackagePath("jui", "src/jui.zig");
+        lib.addPackagePath("jui", "src/jui.zig");
 
-    lib.setTarget(target);
-    lib.setBuildMode(mode);
-    lib.install();
+        lib.setTarget(target);
+        lib.setBuildMode(mode);
+        lib.install();
+    }
+
+    // Tests (it requires a JDK installed)
+    {
+        const java_home = b.env_map.get("JAVA_HOME") orelse @panic("JAVA_HOME not defined.");
+
+        const main_tests = b.addTest("src/jui.zig");
+        main_tests.setBuildMode(mode);
+        main_tests.addLibraryPath(b.pathJoin(&.{ java_home,"/lib/server"}));
+        main_tests.linkSystemLibrary("jvm");
+        main_tests.linkLibC();
+        main_tests.target.abi = .gnu;
+
+        var test_step = b.step("test", "Run library tests");
+        test_step.dependOn(&main_tests.step);
+
+        const argv: []const []const u8 = &.{ b.pathJoin(&.{ java_home,"/bin/javac"}), "test/src/com/jui/TypesTest.java" };
+        _ = b.execFromStep(argv, test_step) catch |err| {
+            std.debug.panic("Fail compiling java files {}", .{err});
+        };
+    }
 }
